a934a3ea8703c5c9fac7e993590b471911e3990d
Лигњослав <SquidwardsGitHub@outlook.com>
2023-06-06 21:49:19 -0500

    USENIX ready

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-COMET_APIKEY = "ZzoZgWBOvF9dwrim8sHzV8dfB" # Change this string to your comet api key
-TASKONOMY_DATASET = "/home/namdd/Documents/github_NOT_ALL_backed_up/paper/taskonomy_small/taskonomy-sample-model-1-small-master"
-BACKUP_OUTPUT_DIR = "/home/namdd/Documents/github_NOT_ALL_backed_up/paper/first_round_picks/"
-SEMANTIC_SEGMENTATION_REBALANCED_ROOT = '/home/namdd/Documents/github_NOT_ALL_backed_up/paper/taskaugment/MTVulnerability/dataloaders/new' 
-PRETRAINED_MTASK_MODEL_PATH = "/home/namdd/Documents/github_NOT_ALL_backed_up/paper/pretrained_models/xception_taskonomy_new_dn.pth.tar"
+COMET_APIKEY = "" # Change this string to your comet api key
+TASKONOMY_DATASET = "path/to/taskonomy_small/taskonomy-sample-model-1-small-master"
+BACKUP_OUTPUT_DIR = "path/to/output/"
+SEMANTIC_SEGMENTATION_REBALANCED_ROOT = 'path/to/taskaugment/MTVulnerability/dataloaders/new' 
+PRETRAINED_MTASK_MODEL_PATH = "path/to/pretrained_models/xception_taskonomy_new_dn.pth.tar"
